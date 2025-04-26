8f41c6dfb72b798cbfb3030ae80f20fa5ecff048
uriy <78085017+monstebit@users.noreply.github.com>
2025-04-26 12:04:43 +0300

    install python-dotenv, add AgentCapabilities

--- src/my_project/__init__.py
+++ src/my_project/__init__.py
@@ -1,17 +1,40 @@
+import logging
+import click
+from dotenv import load_dotenv
 import google_a2a
-from google_a2a.common.types import AgentSkill
+from google_a2a.common.types import AgentSkill, AgentCapabilities, AgentCard
 
-def main():
-  skill = AgentSkill(
-    id="my-project-echo-skill",
-    name="Echo Tool",
-    description="Echos the input given",
-    tags=["echo", "repeater"],
-    examples=["I will see this echoed back to me"],
-    inputModes=["text"],
-    outputModes=["text"],
-  )
-  print(skill)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@click.command()
+@click.option("--host", default="localhost")
+@click.option("--port", default=10002)
+def main(host, port):
+    skill = AgentSkill(
+        id="my-project-echo-skill",
+        name="Echo Tool",
+        description="Echos the input given",
+        tags=["echo", "repeater"],
+        examples=["I will see this echoed back to me"],
+        inputModes=["text"],
+        outputModes=["text"],
+    )
+    logging.info(skill)
+
+    # создание capabilities и agent_card
+    capabilities = AgentCapabilities()
+    agent_card = AgentCard(
+        name="Echo Agent",
+        description="This agent echos the input given",
+        url=f"http://{host}:{port}/",
+        version="0.1.0",
+        defaultInputModes=["text"],
+        defaultOutputModes=["text"],
+        capabilities=capabilities,
+        skills=[skill]
+    )
+    logging.info(agent_card)
 
 if __name__ == "__main__":
-  main()
+    main()
