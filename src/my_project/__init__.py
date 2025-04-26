fcc71f46810f45b0337e8b0f19080cb642782b7e
uriy <78085017+monstebit@users.noreply.github.com>
2025-04-26 11:54:40 +0300

    4. Defining Agent Skills

--- src/my_project/__init__.py
+++ src/my_project/__init__.py
@@ -1,2 +1,17 @@
-def main() -> None:
-    print("Hello from my-project!")
+import google_a2a
+from google_a2a.common.types import AgentSkill
+
+def main():
+  skill = AgentSkill(
+    id="my-project-echo-skill",
+    name="Echo Tool",
+    description="Echos the input given",
+    tags=["echo", "repeater"],
+    examples=["I will see this echoed back to me"],
+    inputModes=["text"],
+    outputModes=["text"],
+  )
+  print(skill)
+
+if __name__ == "__main__":
+  main()
